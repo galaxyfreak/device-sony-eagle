eec01e80820a3f758f6f29c1d447fd0fec7aa0a7
Konstantinos Pliakas <kjpliakas@gmail.com>
2016-07-27 13:39:45 +0300

    Revert "Revert "Remove releasetools""
    This reverts commit 1dcd22de316a3079277d9626583da12c7fa2bbdd.

--- releasetools/sensors.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-id=`cat /sys/class/magnetic/magnetic/ping`
-if [ $id == "0x30:0x06" ]
-then
-	rm -f /system/lib/hw/sensors.msm8226.so
-	mv /system/lib/hw/sensorsecond.msm8226.so /system/lib/hw/sensors.msm8226.so
-fi
-exit 0
