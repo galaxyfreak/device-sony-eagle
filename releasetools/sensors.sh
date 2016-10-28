44d6749f593a6ba490bc5acd035fd64a04f7ea7e
galaxyfreak <marinspajic17@hotmail.com>
2016-10-28 10:28:56 +0200

    sensors.sh: Update for new method
    native patch is no more ;)
    
    Signed-off-by: kpliakas <kjpliakas@gmail.com>

--- releasetools/sensors.sh
+++ releasetools/sensors.sh
@@ -1,7 +1,12 @@
 #!/sbin/sh
-id=$(cat /sys/class/magnetic/magnetic/ping)
-if [ "$id" == "0x30:0x06" ]
+
+DIR="/system/vendor/lib/hw"
+ID=$(cat /sys/class/magnetic/magnetic/ping)
+
+if [ "$ID" == "0x30:0x06" ]
 then
-	echo ro.sensors.sensorsecond=true >> /system/build.prop
+	mount -w -t ext4 /dev/block/platform/msm_sdcc.1/by-name/system /system
+	rm -f $DIR/sensors.eagle.so
+	mv $DIR/sensorsecond.eagle.so $DIR/sensors.eagle.so
 fi
 exit 0
