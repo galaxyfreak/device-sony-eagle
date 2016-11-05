73c5aeed584801ea2f2e654c31901bb63fd9a39e
kpliakas <kjpliakas@gmail.com>
2016-11-05 19:48:13 +0200

    Update variant script Fixed some syntax errors and remove the extra folder creation

--- releasetools/variant.sh
+++ releasetools/variant.sh
@@ -5,13 +5,12 @@ set -e
 # Detect the exact model from the LTALabel partition
 # This looks something like:
 # 1284-8432_5-elabel-D5303-row.html
-mkdir -p /lta-label
-mount -r -t ext4 /dev/block/bootdevice/by-name/LTALabel /lta-label
+mount -r -t ext4 /dev/block/platform/msm_sdcc.1/by-name/LTALabel /lta-label
 variant=`ls /lta-label/*.html | sed s/.*-elabel-// | sed s/-row.html// | tr -d '\n\r'`
 umount /lta-label
  
 # Set the variant as a prop
 touch /system/vendor/build.prop
-echo ro.fxp.variant=${variant^} >> /system/vendor/build.prop
+echo ro.fxp.variant=${variant} >> /system/vendor/build.prop
  
 exit 0
