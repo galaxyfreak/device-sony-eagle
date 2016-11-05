9d3879662a1f042dec299ebb4b68e014e99116f3
kpliakas <kjpliakas@gmail.com>
2016-11-06 00:54:46 +0200

    Final variant.sh (??) HUGE thanks to @galaxyfreak!!!
    -Now it's working :P
    -Correct naming and radio capabilities for each device
    -Correct file permissions
    -Prevent flash errors by deleting the previous build.prop

--- releasetools/variant.sh
+++ releasetools/variant.sh
@@ -8,29 +8,40 @@ set -e
 mount -r -t ext4 /dev/block/platform/msm_sdcc.1/by-name/LTALabel /lta-label
 variant=`ls /lta-label/*.html | sed s/.*-elabel-// | sed s/-row.html// | tr -d '\n\r' | tr '[a-z]' '[A-Z]'`
 umount /lta-label
- 
+
+# Delete old build.prop
+if [ -f /system/vendor/build.prop ]; then
+   rm /system/vendor/build.prop
+fi
+
 # Set the variant as a prop
 touch /system/vendor/build.prop
 echo ro.fxp.variant=${variant} >> /system/vendor/build.prop
 
-# Set Correct product name
-touch /system/vendor/build.prop
-if [ "${variant}" = "D2403" || "${variant}" = "D2406" ]; then
-    echo "ro.product.name=Xperia M2 Aqua" >> /system/vendor/build.prop
-    setprop ro.telephony.ril.config=simactivation
-    setprop ro.telephony.default_network=9
-if [ "${variant}" = "D2302" ]; then
-    echo "ro.product.name=Xperia M2 Dual" >> /system/vendor/build.prop
-    setprop persist.radio.multisim.config=dsds
-    setprop ro.telephony.ril.config=simactivation,sim2gsmonly
-    setprop ro.telephony.default_network=0,1
-if [ "${variant}" = "D2305" ]; then
-    echo "ro.product.name=Xperia M2 3G" >> /system/vendor/build.prop
-    setprop ro.telephony.ril.config=simactivation
-    setprop ro.telephony.default_network=0,1
-else
-    echo "ro.product.name=Xperia M2" >> /system/vendor/build.prop
-    setprop ro.telephony.ril.config=simactivation
-    setprop ro.telephony.default_network=9
+# Set Correct product name and radio capabilities
+if [ "${variant}" = "D2403" ] || [ "${variant}" = "D2406" ]; then
+    echo "ro.product.model=Xperia M2 Aqua" >> /system/vendor/build.prop
+    echo "ro.telephony.ril.config=simactivation" >> /system/vendor/build.prop
+    echo "ro.telephony.default_network=9" >> /system/vendor/build.prop
 fi
+if [ "${variant}" = "D2302" ]; then
+    echo "ro.product.model=Xperia M2 Dual" >> /system/vendor/build.prop
+    echo "persist.radio.multisim.config=dsds" >> /system/vendor/build.prop
+    echo "ro.telephony.ril.config=simactivation,sim2gsmonly" >> /system/vendor/build.prop
+    echo "ro.telephony.default_network=0,1" >> /system/vendor/build.prop
+fi
+if [ "${variant}" = "D2305" ]; then
+    echo "ro.product.model=Xperia M2 3G" >> /system/vendor/build.prop
+    echo "ro.telephony.ril.config=simactivation" >> /system/vendor/build.prop
+    echo "ro.telephony.default_network=0,1" >> /system/vendor/build.prop
+fi
+if [ "${variant}" = "D2303" ] || [ "${variant}" = "D2306" ]; then
+    echo "ro.product.model=Xperia M2" >> /system/vendor/build.prop
+    echo "ro.telephony.ril.config=simactivation" >> /system/vendor/build.prop
+    echo "ro.telephony.default_network=9" >> /system/vendor/build.prop
+fi
+
+# Fix permissions
+chmod 0644 /system/vendor/build.prop
+
 exit 0
