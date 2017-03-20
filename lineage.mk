74955bafc25feb1fb4dc45ad7b76f494b72ba56c
Konstantinos Pliakas <kj77@users.noreply.github.com>
2017-03-20 18:01:11 +0200

    CM is no more! (#4)

--- lineage.mk
+++ lineage.mk
@@ -9,7 +9,7 @@ $(call inherit-product, device/sony/eagle/device.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit CM common Phone stuff.
+# Inherit LineageOS common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Fingerprint for eagle (from stock)
@@ -17,8 +17,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D2303
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D2303/D2303:4.4.4/18.3.1.C.0.21/2n9_bg:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D2303-user 18.3.1.C.0.21 2n9_bg release-keys"
 
-# Override Product Name for CyanogenMod
-PRODUCT_NAME		:= cm_eagle
+# Override Product Name for LineageOS
+PRODUCT_NAME		:= lineage_eagle
 PRODUCT_DEVICE		:= eagle
 PRODUCT_MODEL		:= Xperia M2
 PRODUCT_BRAND		:= Sony
