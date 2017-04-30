d5b77f1d695bb3364f21b59f283751f3f9bebac9
kpliakas <kjpliakas@gmail.com>
2017-04-30 12:35:06 +0300

    Fix WiFi MAC We use a stock hack for this one.Dirty AF,but it works.
     Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -20,7 +20,9 @@ PRODUCT_COPY_FILES := \
     device/sony/eagle/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
     device/sony/eagle/rootdir/system/etc/thermal-engine-8226.conf:system/etc/thermal-engine-8226.conf \
     device/sony/eagle/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    device/sony/eagle/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
+    device/sony/eagle/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
+    device/sony/eagle/rootdir/system/etc/wifi/init.cci.wifi.mac.bootup.sh:system/etc/wifi/init.cci.wifi.mac.bootup.sh \
+    device/sony/eagle/rootdir/system/etc/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Device Specific Permissions
 PRODUCT_COPY_FILES += \
