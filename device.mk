42d0233988c3502b675a0900ac805a5f2cf8ce95
kpliakas <kjpliakas@gmail.com>
2017-11-23 22:35:09 +0200

    Move mixer_paths.xml under /vendor Project treble f*cks with us...other device specific files should follow..

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Device etc
 PRODUCT_COPY_FILES := \
-    device/sony/eagle/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
+    device/sony/eagle/rootdir/system/vendor/etc/mixer_paths.xml:system/vendor/etc/mixer_paths.xml \
     device/sony/eagle/rootdir/system/etc/thermal-engine-8226.conf:system/etc/thermal-engine-8226.conf \
     device/sony/eagle/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
     device/sony/eagle/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
